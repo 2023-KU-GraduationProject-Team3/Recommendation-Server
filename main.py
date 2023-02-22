f30fbe1ec503aa854872e6ef6040b9a151a99c7d
abh <themechanic002@gmail.com>
2023-02-22 19:09:27 +0900

    Update: Get방식에서도 데이터 조회 가능

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 @app.route("/api/content", methods=["GET", "POST"])
 def content():
     if request.method == "POST":
+
         data = request.get_json()
         isbn_num = int(data.get("isbn13"))
         content_result = content_algorithm([isbn_num], 50)
@@ -17,13 +18,24 @@ def content():
         if isbn_num:
             return jsonify(content_result), 201
         else:
-            return jsonify({"message": "Error"}), 400
+            return jsonify({"message": "Failed to bring data"}), 400
+
+    elif request.method == "GET":
+
+        isbn_num = int(request.args.get("isbn_num"))
+        content_result = content_algorithm([isbn_num], 50)
+
+        if isbn_num:
+            return jsonify(content_result), 201
+        else:
+            return jsonify({"message": "Failed to bring data"}), 400
     else:
-        return jsonify({"message": "Success! Request method is GET!"}), 200
+        return jsonify({"message": "Wrong access"}), 200
 
 @app.route("/api/collab", methods=["GET", "POST"])
 def collab():
     if request.method == "POST":
+
         data = request.get_json()
         user_id = int(data.get("user_id"))
         collab_result = collab_algorithm(user_id, 3)
@@ -31,9 +43,19 @@ def collab():
         if user_id:
             return jsonify(collab_result), 201
         else:
-            return jsonify({"message": "Error"}), 400
+            return jsonify({"message": "Failed to bring data"}), 400
+
+    elif request.method == "GET":
+
+        user_id = int(request.args.get("user_id"))
+        collab_result = collab_algorithm(user_id, 3)
+
+        if user_id:
+            return jsonify(collab_result), 201
+        else:
+            return jsonify({"message": "Failed to bring data"}), 400
     else:
-        return jsonify({"message": "Success! Request method is GET!"}), 200
+        return jsonify({"message": "Wrong access"}), 200
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port=5000)
