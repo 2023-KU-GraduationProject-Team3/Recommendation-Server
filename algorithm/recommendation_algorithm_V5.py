2b32aa9cda407206821042c773f0d778ddfca9a9
themechanic002 <themechanic002@gmail.com>
2023-03-23 15:26:26 +0900

    Update: 협업필터링 깔끔하게 수정

--- algorithm/recommendation_algorithm_V5.py
+++ algorithm/recommendation_algorithm_V5.py
@@ -6,6 +6,8 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import TfidfVectorizer
+from surprise import Reader, Dataset, SVD
+import json
 
 from scipy.sparse.linalg import svds
 import warnings
@@ -13,105 +15,48 @@ import warnings
 
 warnings.filterwarnings("ignore")
 
-def collab_algorithm(user_id, num_recommendations=5):
-    df_ratings = pd.read_csv('res/ratings_final_ISBN3.csv')  # 유저랑 동일한 성별 및 비슷한 나이대로 필터링 하고난 ratings.csv 사용
-    df_books = pd.read_csv('res/popular_books_ISBN3.csv')
+def collab_algorithm(user_id, num_books=10):
+    # rating csv 파일 일기
+    ratings_df = pd.read_csv('res/ratings_final_ISBN3.csv')
 
-    df_user_movie_ratings = df_ratings.pivot(
-        index='userId',
-        columns='isbn13',
-        values='rating'
-    ).fillna(0)
+    # 도서 csv 파일 일기
+    books_df = pd.read_csv('res/popular_books_ISBN3.csv')
 
-    # df_user_movie_ratings.head(3)
-    # 사용자 - 도서 pivot table 제작
+    # reader object 생성
+    reader = Reader(rating_scale=(1, 5))
 
-    # matrix는 pivot_table 값을 numpy matrix로 만든 것
-    matrix = df_user_movie_ratings.values
+    # surprise Dataset에 reader 넣기
+    data = Dataset.load_from_df(ratings_df[['userId', 'isbn13', 'rating']], reader)
 
-    # user_ratings_mean은 사용자의 평균 평점 (평을 안 남긴 유저들도 있기 때문)
-    user_ratings_mean = np.mean(matrix, axis=1)
+    # SVD model
+    svd = SVD()
+    trainset = data.build_full_trainset()
+    svd.fit(trainset)
 
-    # R_user_mean : 사용자-도서에 대해 사용자 평균 평점을 뺀 것.
-    matrix_user_mean = matrix - user_ratings_mean.reshape(-1, 1)
-    matrix
+    # 모든 책을 isbn으로 가져오기
+    all_books = list(set(ratings_df['isbn13'].unique()).intersection(set(books_df['isbn13'].unique())))
 
-    pd.DataFrame(matrix_user_mean, columns=df_user_movie_ratings.columns).head()
+    # 입력 받은 user에 대해 도서 isbn과 평점 리스트 생성
+    user_estimates = []
+    for book_id in all_books:
+        user_estimates.append((book_id, svd.predict(user_id, book_id).est))
 
-    # svd 를 이용해 matrix factorization
-    # svd : 특이값 분해
-    # -> m * n 크기의 데이터 행렬 A를 분해
+    # 평점 내림차순으로 리스트 정렬하기
+    sorted_estimates = sorted(user_estimates, key=lambda x: x[1], reverse=True)
 
-    # scipy에서 제공해주는 svd.
-    # U 행렬, sigma 행렬, V 전치 행렬을 반환.
+    # 위에서부터 n개 도서 가져오기
+    recommended_books = []
+    for i in range(num_books):
+        isbn = sorted_estimates[i][0]
+        bookname = books_df.loc[books_df['isbn13'] == isbn, 'bookname'].iloc[0]
+        recommended_books.append({"isbn13": int(isbn), "bookname": str(bookname)})
 
-    U, sigma, Vt = svds(matrix_user_mean, k=5)
-
-    # print(U.shape)
-    # print(sigma.shape)
-    # print(Vt.shape)
-    sigma = np.diag(sigma)
-    sigma.shape
-    sigma[0]
-    sigma[1]
-    # 0 이 포함된 대칭행렬로 변환하기 위해 numpy의 diag를 이용함
-
-    # U, Sigma, Vt의 내적을 수행하면, 다시 원본 행렬로 복원이 된다.
-    # 내적 => np.dot(np.dot(U, sigma), Vt) 수행
-    # 거기에 + 사용자 평균 rating을 적용한다.
-    svd_user_predicted_ratings = np.dot(np.dot(U, sigma), Vt) + user_ratings_mean.reshape(-1, 1)
-
-    df_svd_preds = pd.DataFrame(svd_user_predicted_ratings, columns=df_user_movie_ratings.columns)
-    df_svd_preds.head()
-
-    df_svd_preds.shape
-
-    return calculate(df_svd_preds, user_id, df_books, df_ratings, num_recommendations).to_json(orient="index", force_ascii=False)
+    return json.dumps(recommended_books, ensure_ascii=False)
 
 
-
-# 1. 행렬 분해를 활용한 협업 필터링 #
-# 사용자 개인 도서 히스토리를 기반으로 도서추천 #
-
-def calculate(df_svd_preds, user_id, ori_books_df, ori_ratings_df, num_recommendations=5):
-
-    # 현재는 index로 적용이 되어있으므로 user_id - 1을 해야함.
-    user_row_number = user_id - 1
-
-    # 최종적으로 만든 pred_df에서 사용자 index에 따라 도서 데이터 정렬 -> 도서 평점이 높은 순으로 정렬 됌
-    sorted_user_predictions = df_svd_preds.iloc[user_row_number].sort_values(ascending=False)
-
-    # 원본 평점 데이터에서 user id에 해당하는 데이터를 뽑아낸다.
-    user_data = ori_ratings_df[ori_ratings_df.userId == user_id]
-
-    # 위에서 뽑은 user_data와 원본 도서 데이터를 합친다.
-    user_history = user_data.merge(ori_books_df, on='isbn13').sort_values(['rating'], ascending=False)
-
-    # 원본 도서 데이터에서 사용자가 본 도서 데이터를 제외한 데이터를 추출
-    recommendations = ori_books_df[~ori_books_df['isbn13'].isin(user_history['isbn13'])]
-    # 사용자의 도서 평점이 높은 순으로 정렬된 데이터와 위 recommendations을 합친다.
-    recommendations = recommendations.merge(pd.DataFrame(sorted_user_predictions).reset_index(), on='isbn13')
-    # 컬럼 이름 바꾸고 정렬해서 return
-    recommendations = recommendations.rename(columns={user_row_number: 'Predictions'}).sort_values('Predictions',
-                                                                                                   ascending=False).iloc[
-                      :num_recommendations, :]
-
-    already_rated = user_history
-    predictions = recommendations
-
-    # 원하는 유저의 userId(인덱스) 넣어주기
-    already_rated.head(10)
-
-    return predictions
-
-
-
-
-print("----- 협업 필터링 결과 ------")
-print(collab_algorithm(7, 3))  # 협업 필터링 최종 결과
-print("-----------------------------")
-print()
-print()
+#print("----- 협업 필터링 결과 ------")
+#print(str(collab_algorithm(2, 3)))  # 협업 필터링 최종 결과
+#print("-----------------------------")
 
 
 
