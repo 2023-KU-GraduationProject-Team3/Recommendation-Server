d1d5418f61e0824d46f2b195426b437386f2598c
themechanic002 <themechanic002@gmail.com>
2023-04-02 21:27:33 +0900

    Add&Delete: 쓸모 없는 파일 삭제 및 실제 DB파일 추가

--- sqlite_create.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('res/book.db')
-conn.execute('CREATE TABLE POPULAR_BOOKS (NAME TEXT, ADDR TEXT, CITY TEXT, PIN TEXT)')
-print('Table created successfully')
-conn.close()
